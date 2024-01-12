235c4397dc17fdd48a883a06ee6979b631af230c
Ad4mWest <https://github.com/Ad4mWest>
2024-01-12 21:55:30 +0300

    Initial Commit

---
+++ OrderFood/OrderFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderFoodApp.swift
+//  OrderFood
+//
+//  Created by Adam West on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct OrderFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
