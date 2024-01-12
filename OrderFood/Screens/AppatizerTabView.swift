c18bb0ad8822667ea19c14b0d4b1cc7f4935f809
Ad4mWest <https://github.com/Ad4mWest>
2024-01-12 22:42:03 +0300

    Restructure project.

---
+++ OrderFood/Screens/AppatizerTabView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Appotizers
+//
+//  Created by Adam West on 11.01.2024.
+//
+
+import SwiftUI
+
+struct AppatizerTabView: View {
+    var body: some View {
+        TabView {
+            AppetizerListView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            OrderView()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
+        }
+        .tint(Color.brandP) // color of tab item
+    }
+}
+
+#Preview {
+    AppatizerTabView()
+}
